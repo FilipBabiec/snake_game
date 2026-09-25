ea57d48513d5647d85483200b86c07aa30d800ad
FilipBabiec <filipbabiec@gmail.com>
2026-09-25 08:49:23 +0200

    Added high score mechanic

--- scoreboard.py
+++ scoreboard.py
@@ -12,16 +12,23 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
+        with open("highscore.txt", mode="r") as f:
+            self.highscore = int(f.read())
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}    High score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def game_over(self):
         self.goto(0, 0)
         self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+        with open("highscore.txt", mode="w") as f:
+            f.write(str(self.highscore))
+        self.score = 0
 
     def increase_score(self):
         self.score += 1
+        if self.score > self.highscore:
+            self.highscore = self.score
         self.clear()
         self.update_scoreboard()
